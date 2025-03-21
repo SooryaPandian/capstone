92dbdb5015ca72e061b6fa024f50763eddfed29e
Sooryapandian S P <sooryapandian37@gmail.com>
2025-03-21 11:46:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
+    sudo su
     docker login -u sooryapandian -p dckr_pat_hEHO3X3SS_zZEskGdkx9VzAPF2o
     docker tag test sooryapandian/sample
     docker push sooryapandian/sample
